651af0a98f7e75217b2c2851a2725b1c1f28c21d
Jason <pewsheen@gmail.com>
2017-06-07 18:32:17 +0800

    asdfasdf

--- daos.lua
+++ daos.lua
@@ -7,7 +7,7 @@ local SCHEMA = {
 		id = {type = "id", dao_insert_value = true}, -- a value to be inserted by the DAO itself (think of serial ID and the uniqueness of such required here)
 		created_at = {type = "timestamp", immutable = true, dao_insert_value = true}, -- also interted by the DAO itself
 		public_key = {type = "string", required = true, unique = false}, -- a unique API key
-		kerker = {type = "id", required = true, unique = false, default = utils.random_string}
+		kerker = {type = "string", required = true, unique = false, default = utils.random_string}
 	},
 	marshall_event = function(self, t)
 		return { id = t.id, public_key = t.public_key, kerker = t.kerker }
