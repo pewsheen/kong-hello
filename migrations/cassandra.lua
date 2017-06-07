013eebf829755f4488d7659aa85082a2921e2d40
Jason <pewsheen@gmail.com>
2017-06-07 10:55:13 +0800

    sad

--- migrations/cassandra.lua
+++ migrations/cassandra.lua
@@ -1,15 +1,15 @@
 return {
 {
-	name = "2017-06-06-172400_init_hello",
+	name = "2017-06-06-172401_init_hello",
 	up =  [[
 		CREATE TABLE IF NOT EXISTS hello_woorld(
 		id uuid,
-		puclic_key text,
+		public_key text,
 		created_at timestamp,
 		PRIMARY KEY (id)
 		);
 
-		CREATE INDEX IF NOT EXISTS ON hello_woorld(puclic_key);
+		CREATE INDEX IF NOT EXISTS ON hello_woorld(public_key);
 	]],
 	down = [[
 		DROP TABLE hello_woorld;
--- migrations/postgres.lua
+++ migrations/postgres.lua
@@ -1,10 +1,10 @@
 return {
 {
-	name = "2015-07-31-172400_init_hello",
+	name = "2015-07-31-172401_init_hello",
 	up = [[
 		CREATE TABLE IF NOT EXISTS hello_woorld(
 		id uuid,
-		puclic_key text UNIQUE,
+		public_key text UNIQUE,
 		created_at timestamp without time zone default (CURRENT_TIMESTAMP(0) at time zone 'utc'),
 		PRIMARY KEY (id)
 		);
@@ -12,7 +12,7 @@ return {
 		DO $$
 		BEGIN
 		IF (SELECT to_regclass('public.keyauth_key_idx')) IS NULL THEN
-			CREATE INDEX keyauth_key_idx ON hello_woorld(puclic_key);
+			CREATE INDEX keyauth_key_idx ON hello_woorld(public_key);
 		END IF;
 		END$$;
 	]],
